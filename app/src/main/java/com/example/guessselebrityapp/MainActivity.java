a9920b7604f6b9d9967fa6d4581ba5024f5de882
amarez <muhammadamarez@gmail.com>
2022-10-31 13:34:43 +0700

    new Button
    new Button

--- app/src/main/java/com/example/guessselebrityapp/MainActivity.java
+++ app/src/main/java/com/example/guessselebrityapp/MainActivity.java
@@ -3,16 +3,17 @@ package com.example.guessselebrityapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void createUser() {
-
-    }
+    Button correctButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        correctButton = findViewById(R.id.button)
     }
 }
